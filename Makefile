682afcdcb9c916f8fd7449d48b921172119f3ca7
Marcus Westin <marcus.westin@gmail.com>
2010-08-09 20:52:07 -0700

    Import the syntax highlighter from fun

--- Makefile
+++ Makefile
@@ -15,8 +15,12 @@ posts:
 	node posts/compile.js
 
 .PHONY: update-store.js
-lib/store.js:
+update-store.js:
 	curl http://github.com/marcuswestin/store.js/raw/master/store.js > lib/store.js
+
+.PHONY: update-syntaxHighligther.js
+update-funSyntaxHighligther.js:
+	curl http://github.com/marcuswestin/fun/raw/master/syntaxHighlighter.js > lib/funSyntaxHighligther.js
 
 .PHONY: clean
 clean:
--- posts/compile.js
+++ posts/compile.js
@@ -2,7 +2,8 @@ if (__dirname != 'posts') { process.chdir('posts') }
 
 var fs = require('fs'),
 	sys = require('sys'),
-	Showdown = require('../write/showdown')
+	Showdown = require('../write/showdown'),
+	funSyntaxHighlighter = require('../lib/funSyntaxHighlighter')
 
 function p(o) { sys.puts(JSON.stringify(o)) }
 function puts(o) { sys.puts(o) }
@@ -60,14 +61,7 @@ function syntaxHighlight(html) {
 		}
 		
 		if (inCode) {
-			line = line
-				.replace(/"([^"]*)"/g, function(m) { return '<span class="string">'+m+'</span>'})
-				.replace(/(let|for|in|if|else) /g, function(m) { return '<span class="keyword">'+m+'</span>'})
-				.replace(/(handler) ?/g, function(m) { return '<span class="type">'+m+'</span>'})
-				.replace(/(Session|Local|Global)/g, function(m) { return '<span class="global">'+m+'</span>'})
-				.replace(/(data|onClick)=/g, function(m,m2) { return '<span class="attr">'+m2+'</span>='})
-				.replace(/(&lt;|&gt;)/g, function(m) { return '<span class="xml">'+m+'</span>'})
-				.replace(/ = /g, '<span class="operator"> = </span>')
+			line = funSyntaxHighlighter.highlightLine(line)
 		}
 		
 		newLines.push(line)
