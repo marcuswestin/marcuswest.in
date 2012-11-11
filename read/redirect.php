d817ec260367525c4c0a8e67145ceb641e655937
Marcus Westin <marcus.westin@gmail.com>
2012-11-10 19:19:04 -0800

    fix redirect

--- read/redirect.php
+++ read/redirect.php
@@ -2,6 +2,6 @@
 
 $newPath = preg_replace("/^\/read\//i", "/on/", $_SERVER["REQUEST_URI"]);
 header("HTTP/1.1 301 Moved Permanently");
-header("Location: http://marcuswest.in$$newPath");
+header("Location: http://marcuswest.in$newPath");
 
 ?>
