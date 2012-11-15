fa262e8029fc11e282067f1be9ed48035c1b9818
Marcus Westin <marcus.westin@gmail.com>
2012-11-15 14:05:46 -0800

    move marcuswest.in/on/* to marcuswest.in/essays/*

--- on/redirect.php
+++ on/redirect.php
@@ -1,6 +1,6 @@
 <?php
 
-$newPath = preg_replace("/^\/read\//i", "/essays/", $_SERVER["REQUEST_URI"]);
+$newPath = preg_replace("/^\/on\//i", "/essays/", $_SERVER["REQUEST_URI"]);
 header("HTTP/1.1 301 Moved Permanently");
 header("Location: http://marcuswest.in$newPath");
 
